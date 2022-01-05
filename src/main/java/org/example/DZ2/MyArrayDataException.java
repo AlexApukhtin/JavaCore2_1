5fd7e7651d62805c584ac6bfc42ca2af9737e8f5
AlexApukhtin <aac94@yandex.ru>
2022-01-05 03:44:37 +0300

    DZ1.task1,2,4

--- src/main/java/org/example/DZ2/MyArrayDataException.java
+++ src/main/java/org/example/DZ2/MyArrayDataException.java
@@ -3,11 +3,11 @@ package org.example.DZ2;
 public class MyArrayDataException extends RuntimeException{
 
 
-    private static String MESSAGE = "Ошибка в %s-ой строке";
+    private static String MESSAGE = "В массиве есть элемент типа String. Его координаты: i=%s; j=%d";
     private int i;
 
     public MyArrayDataException(int i, int j) {
-        super(String.format(MESSAGE, i));
+        super(String.format(MESSAGE, i, j));
     }
 
     public MyArrayDataException(String message, Throwable cause) {
