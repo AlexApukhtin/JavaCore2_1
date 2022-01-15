fba1c370d200b235eb4b116ea438f9ce0b1a9066
AlexApukhtin <aac94@yandex.ru>
2022-01-15 16:33:09 +0300

    DZ3 task1,2

---
+++ src/main/java/org/example/People.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class People {
+
+    private String surname;
+    private String phone;
+
+    public People(String phone, String surname) {
+        this.surname = surname;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "surname='" + surname + '\'' +
+                ", phone='" + phone + '\'' +
+                '}'+ '\n';
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
